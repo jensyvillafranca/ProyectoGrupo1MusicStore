ba3ed9257032c3f469f48e70fb4b6248f60fbcf7
jvillafranca <jensylorena2402@gmail.com>
2023-10-21 23:28:45 -0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProyectoGrupo1MusicStore"
+include(":app")
+ 
