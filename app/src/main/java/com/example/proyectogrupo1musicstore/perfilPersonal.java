f3c95ad771ee698456c980a3ae3642c52b52af89
kdelwin <cnombre982@gmail.com>
2023-10-22 20:29:53 -0600

    Kevin

---
+++ app/src/main/java/com/example/proyectogrupo1musicstore/perfilPersonal.java
@@ -0,0 +1,14 @@
+package com.example.proyectogrupo1musicstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class perfilPersonal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_personal);
+    }
+}
