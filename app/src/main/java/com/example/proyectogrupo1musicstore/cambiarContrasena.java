4fcb9aee48acc0c0b1d4d3f651e9b319f47cc48c
kdelwin <cnombre982@gmail.com>
2023-10-22 23:11:24 -0600

    Kevin

---
+++ app/src/main/java/com/example/proyectogrupo1musicstore/cambiarContrasena.java
@@ -0,0 +1,14 @@
+package com.example.proyectogrupo1musicstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cambiarContrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambiar_contrasena);
+    }
+}
