63d371ffc4c4b67beb7795bd3af01ac56d7c253a
jvillafranca <jensylorena2402@gmail.com>
2023-10-24 16:19:15 -0600

    Pantalla Login Finalizando Errores parte 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.proyectogrupo1musicstore"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.proyectogrupo1musicstore"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/proyectogrupo1musicstore/MainActivity.java
+++ app/src/main/java/com/example/proyectogrupo1musicstore/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.proyectogrupo1musicstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button prueba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prueba = (Button) findViewById(R.id.prueba);
+
+        prueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent prueba = new Intent(getApplicationContext(),activity_login.class);
+                startActivity(prueba);
+            }
+        });
     }
 }
