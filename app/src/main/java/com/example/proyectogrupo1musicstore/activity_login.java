acf2b3f459376a3f3067218063e50336ba38d56e
jvillafranca <jensylorena2402@gmail.com>
2023-10-25 01:09:45 -0600

    Solucionando actualización Jensy

--- app/src/main/java/com/example/proyectogrupo1musicstore/activity_login.java
+++ app/src/main/java/com/example/proyectogrupo1musicstore/activity_login.java
@@ -10,5 +10,6 @@ public class activity_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
