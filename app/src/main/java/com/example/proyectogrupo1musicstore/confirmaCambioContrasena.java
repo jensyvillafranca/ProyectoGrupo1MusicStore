54d6e9f6331012402c16e907e61e0398dd5c6b4b
claudia.rodriguez <pazclaudia2019@gmail.com>
2023-11-01 22:33:45 -0600

    Claudia - Kevin

---
+++ app/src/main/java/com/example/proyectogrupo1musicstore/confirmaCambioContrasena.java
@@ -0,0 +1,14 @@
+package com.example.proyectogrupo1musicstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class confirmaCambioContrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirma_cambio_contrasena);
+    }
+}
