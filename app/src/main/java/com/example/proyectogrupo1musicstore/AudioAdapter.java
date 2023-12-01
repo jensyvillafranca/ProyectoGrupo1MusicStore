a2e8684acae8ba86fb1c78ab9ddde0c7274294d0
JonathanMarley <jonathanmarley2536@gmail.com>
2023-11-30 23:17:07 -0600

    modificacion de Adapter Audio y reproductor.

--- app/src/main/java/com/example/proyectogrupo1musicstore/AudioAdapter.java
+++ app/src/main/java/com/example/proyectogrupo1musicstore/AudioAdapter.java
@@ -40,7 +40,8 @@ public class AudioAdapter extends RecyclerView.Adapter<AudioAdapter.AudioViewHol
         audioItem item = itemList.get(position);
         holder.audioImage.setImageBitmap(item.getImageResId());
         holder.audioName.setText(item.getItemName());
-       //esta parte fue modificado por JM
+
+        //parte fue modificado por JM
         ImageView imgAudio = holder.itemView.findViewById(R.id.itemPortadaAudios);
 
 
