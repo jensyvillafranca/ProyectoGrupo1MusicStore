2404cd4cef4f21ea8b9d8e8aa8dbff1137fc3f88
Gustavo <tavo198704@gmail.com>
2023-12-01 00:41:29 -0600

    Agrega ventana acerca de

---
+++ app/src/main/java/com/example/proyectogrupo1musicstore/Activities/AcercaDe/ActivityAcercaDe.java
@@ -0,0 +1,16 @@
+package com.example.proyectogrupo1musicstore.Activities.AcercaDe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyectogrupo1musicstore.R;
+
+public class ActivityAcercaDe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+    }
+}
