ef57749b8b7b4fe0c96e6a19a0d946f2a736947c
JonathanMarley <jonathanmarley2536@gmail.com>
2023-12-01 22:50:38 -0600

    ultimas pantallas de la playlist,modificaciones

--- app/src/main/java/com/example/proyectogrupo1musicstore/ActivityVerPlayList.java
+++ app/src/main/java/com/example/proyectogrupo1musicstore/ActivityVerPlayList.java
@@ -1,7 +1,9 @@
 package com.example.proyectogrupo1musicstore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
@@ -12,20 +14,30 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.proyectogrupo1musicstore.Activities.Grupos.ActivityVerTodosIntegrantes;
+import com.example.proyectogrupo1musicstore.Adapters.CustomAdapterPlay;
+import com.example.proyectogrupo1musicstore.Adapters.CustomAdapterReproductorPlaylist;
 import com.example.proyectogrupo1musicstore.Adapters.PlayListAdapter;
 import com.example.proyectogrupo1musicstore.Models.PlayListItem;
+import com.example.proyectogrupo1musicstore.Models.vistadeplaylist;
+import com.example.proyectogrupo1musicstore.NetworkTaksMulti.FetchDataAsyncModiPlayList;
 import com.example.proyectogrupo1musicstore.NetworkTaksMulti.ObtenerPlayListAsyncTask;
 import com.example.proyectogrupo1musicstore.NetworkTaksMulti.informacionGeneralPlayListAstAsyncTask;
 import com.example.proyectogrupo1musicstore.NetworkTasks.obtenerIntegrantesGrupoAsyncTask;
+import com.example.proyectogrupo1musicstore.Utilidades.Token.JwtDecoder;
+import com.example.proyectogrupo1musicstore.Utilidades.Token.token;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ActivityVerPlayList extends AppCompatActivity {
+public class ActivityVerPlayList extends AppCompatActivity implements FetchDataAsyncModiPlayList.DataFetchListener {
     ImageButton botonAtrass;
+    DrawerLayout drawerLayout;
     RecyclerView recyclerviewvertodoPlayList;
     ProgressDialog progressDialog;
     private int idplaylist;
+
+    private com.example.proyectogrupo1musicstore.Utilidades.Token.token token = new token(this);
+    private int idUsuario;
 
     private final String tipo = "1";
     @Override
@@ -36,24 +48,25 @@ public class ActivityVerPlayList extends AppCompatActivity {
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Cargando...");
         progressDialog.setCancelable(false);
-        progressDialog.show();
+        //progressDialog.show();
 
-        idplaylist = 2;
+
         recyclerviewvertodoPlayList  = (RecyclerView) findViewById(R.id.recyclerview_vertodoPlayList);
         botonAtrass = (ImageButton) findViewById(R.id.btn_vertodosPlayListAtras);
+       // drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layoutsVerPlaylistAgregar);
+        recyclerviewvertodoPlayList = (RecyclerView) findViewById(R.id.recyclerview_vertodoPlayList);
 
-        // Creación de una lista de elementos de integrantesItem
-        List<PlayListItem> playlistitemList = new ArrayList<>();
-        // Crea y vincula el adaptador - playadapter
-        PlayListAdapter playAdapter = new PlayListAdapter(this, playlistitemList);
-        recyclerviewvertodoPlayList.setAdapter(playAdapter);
+        idUsuario = Integer.parseInt(JwtDecoder.decodeJwt(token.recuperarTokenFromKeystore()));
 
-        //Configuracion del administrador de diseño playlist
-        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+
+        String url = "https://phpclusters-152621-0.cloudclusters.net/modiPlayList.php";
+        //Remplazar por el idusuario - motivos de pruebas
+        progressDialog.show();
+        new FetchDataAsyncModiPlayList(this, progressDialog).execute(url, String.valueOf(idUsuario));
+
+        // Configuración del administrador de diseño y adaptador para el RecyclerView
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this); // Use an appropriate layout manager
         recyclerviewvertodoPlayList.setLayoutManager(layoutManager);
-
-        new ObtenerPlayListAsyncTask(ActivityVerPlayList.this, playAdapter, progressDialog)
-                .execute(String.valueOf(idplaylist), tipo);
 
         View.OnClickListener buttonClick = new View.OnClickListener() {
             @Override
@@ -72,11 +85,16 @@ public class ActivityVerPlayList extends AppCompatActivity {
         botonAtrass.setOnClickListener(buttonClick);
     }
 
+    public void onDataFetched(List<vistadeplaylist> dataList) {
+        // Muestra el Recycle view con la nueva informacion
+        progressDialog.dismiss(); // Esconde el spinner de carga
+        CustomAdapterReproductorPlaylist adapter = new CustomAdapterReproductorPlaylist(this, dataList, idUsuario, recyclerviewvertodoPlayList);
+        recyclerviewvertodoPlayList.setAdapter(adapter);
+    }
+
     private void moveActivity(Class<?> actividad) {
         Intent intent = new Intent(getApplicationContext(), actividad);
-        intent.putExtra("idplaylist", idplaylist);
         startActivity(intent);
-
     }
 
 }
---
+++ app/src/main/java/com/example/proyectogrupo1musicstore/NetworkTaksMulti/BuscarAudiosAsyncTask.java
@@ -0,0 +1,145 @@
+package com.example.proyectogrupo1musicstore.NetworkTaksMulti;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.proyectogrupo1musicstore.Adapters.CustomAdapterBuscarGrupos;
+import com.example.proyectogrupo1musicstore.Adapters.CustomAdapterMusicaVideos;
+import com.example.proyectogrupo1musicstore.Models.buscarAudioMusica;
+import com.example.proyectogrupo1musicstore.Models.buscarGrupo;
+import com.example.proyectogrupo1musicstore.Utilidades.Imagenes.ImageDownloader;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuscarAudiosAsyncTask extends AsyncTask<String, Void, Pair<Integer, List<buscarAudioMusica>>> {
+    private static final String TAG = "BuscarAudiosAsyncTask";
+    private Context context;
+    private RecyclerView recyclerView;
+    private CustomAdapterMusicaVideos adapter;
+    private ProgressDialog progressDialog;
+
+    public BuscarAudiosAsyncTask(Context context, RecyclerView recyclerView, CustomAdapterMusicaVideos adapter) {
+        this.context = context;
+        this.recyclerView = recyclerView;
+        this.adapter = adapter;
+        this.progressDialog = progressDialog;
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Buscando...");
+        progressDialog.setCancelable(false);
+    }
+
+    @Override
+    protected Pair<Integer, List<buscarAudioMusica>> doInBackground(String... params) {
+        String idusuario = params[0];
+        String search = params[1];
+
+        try {
+            URL url = new URL("https://phpclusters-152621-0.cloudclusters.net/busccarCanciones.php");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setDoOutput(true);
+
+            JSONObject jsonRequest = new JSONObject();
+            jsonRequest.put("nombrecancion", search);
+            jsonRequest.put("idusuario", idusuario);
+
+            OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+            out.write(jsonRequest.toString().getBytes());
+            out.flush();
+            out.close();
+
+            int responseCode = urlConnection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                InputStream inputStream = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+                inputStream.close();
+
+                List<buscarAudioMusica> result = parseJsonResponse(response.toString());
+                return new Pair<>(responseCode, result);
+            } else {
+                Log.e(TAG, "Error response code: " + responseCode);
+            }
+
+            urlConnection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new Pair<>(HttpURLConnection.HTTP_INTERNAL_ERROR, null);
+    }
+
+    @Override
+    protected void onPostExecute(Pair<Integer, List<buscarAudioMusica>> result) {
+        progressDialog.dismiss();
+
+        int responseCode = result.first;
+        List<buscarAudioMusica> data = result.second;
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            // Parse and process the JSON data
+            if (data != null) {
+                adapter.setDataList(data);
+                adapter.notifyDataSetChanged();
+            }
+        } else if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
+            // No groups found, show a toast
+            Toast.makeText(context, "¡No se encontró ninguna Musical!", Toast.LENGTH_SHORT).show();
+        } else {
+            // Handle other response codes or errors
+            Toast.makeText(context, "¡No se encontró ninguna musica!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private List<buscarAudioMusica> parseJsonResponse(String json) {
+        List<buscarAudioMusica> dataList = new ArrayList<>();
+
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                // Extrae la informacion y crea objetos
+                Integer idaudio = jsonObject.getInt("idaudio");
+                String nombre = jsonObject.getString("nombreCancion");
+                String autorcancion = jsonObject.getString("autor");
+                Bitmap imageResource = ImageDownloader.downloadImage(jsonObject.getString("enlacefoto"));
+                String genero = jsonObject.getString("genero");
+
+                dataList.add(new buscarAudioMusica(idaudio, nombre, autorcancion,imageResource, genero));
+            }
+
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing JSON response: " + e.getMessage());
+        }
+        return dataList;
+    }
+}
